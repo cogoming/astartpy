eb6fa019472a850de91d7e36d447e7ae0833d51c
qiuming@mq.com <qiuming@mq.com>
2017-12-01 17:02:24 +0800

    update alien_invasion

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -15,7 +15,7 @@ def run_game():
     pygame.init();
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
+    pygame.display.set_caption("大空护卫")
 
     # 创建Play按钮
     play_button = Button(ai_settings, screen, "Play")
